6f3c279b90863e9848f2613ad6bf9f18a907717f
StianLervik <stian.lervik@outlook.com>
2016-04-24 22:58:20 +0200

    Spel

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    public float maxFart;
+
+    private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(moveSpeed*Input.GetAxis("Horizontal")* Time.deltaTime, 0.0f, moveSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 fart = Input.GetAxis("Vertical") * Vector3.forward * moveSpeed * Time.fixedDeltaTime * 100.0f;
+        if (body.velocity.magnitude+fart.magnitude <= maxFart)
+        {
+            body.AddRelativeForce(fart);
+        }
+        else
+        {
+            body.velocity = Vector3.forward * maxFart;
+        }
+        //body.AddRelativeForce(Input.GetAxis("Horizontal")*Vector3.right*moveSpeed*Time.fixedDeltaTime * 100);
+    }
+
+}
